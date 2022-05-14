111af9867752a404a19cfe53f81a7629252ed841
Thariq Fahry <37890989+thariqfahry@users.noreply.github.com>
2022-05-14 04:07:41 +0100

    Add inversion logic and comments
    - Added logic to invert back the active-low outputs from MiniProject.
    
    - Added pressAndRelease task.

--- simulation/MiniProject_tb.v
+++ simulation/MiniProject_tb.v
@@ -1,29 +1,30 @@
-// Timescale indicates unit of delays.
-//  `timescale  unit / precision
-// Where delays are given as:
-//   #unit.precision
-//
-// For the clock generator in this test bench to work, we must
-// stick with a "unit" of 1ns. You may choose the "precision".
-//
-// e.g for `timescale 1ns/100ps then:
-//   #1 = 1ns
-//   #1.5 = 1.5ns
-//   #1.25 = 1.3ns (rounded to nearest precision)
+/*
+ * MiniProject - ColourMemory2000 Test bech
+ * ----------------------------
+ * By: 201158825 Thariq Fahry
+ * Date: 14th April 2022
+ *
+ * Description
+ * ------------
+ * A test bench for the Mini-Project module.
+ *  
+ * This is based on the code skeleton in the Unit 3 LT24Top_tb:
+ * https://github.com/leeds-embedded-systems/ELEC5566M-Unit3-thariqfahry/blob/main/3-2-LT24TestProject/simulation/LT24Top_tb.v
+ *
+ */
+
 `timescale 1 ns/100 ps
 
-// Test bench module declaration
-//  Always end test bench module names with _tb for clarity
-//  There is no port list for a test bench
 module MiniProject_tb;
 
 //
 // Parameter Declarations
 //
-localparam NUM_CYCLES = 10000000;   //Run simulation for NUM_CYCLES clock cycles. (Max 1 billion)
-localparam CLOCK_FREQ = 50000000; //Clock frequency
+localparam NUM_CYCLES = 5000000;
+localparam CLOCK_FREQ = 5000;        // Clock frequency. Must be equal to the SECOND parameter in MiniProject.v.
 
-localparam NUM_FRAMES = 2;
+localparam NUM_FRAMES = 3;           // Number of frames to render to write.txt before ending the simulation.
+
 
 //
 // Test Bench Generated Signals
@@ -31,10 +32,20 @@ localparam NUM_FRAMES = 2;
 reg  clock;
 reg  reset;
 
+reg [3:0] key = 4'b0000;
+wire[3:0] n_key = ~key;
+
 //
 // DUT Output Signals
 //
 wire resetApp;
+
+wire [6:0] n_sevenseg0;
+wire [6:0] n_sevenseg1;
+wire [6:0] sevenseg0 = ~n_sevenseg0;
+wire [6:0] sevenseg1 = ~n_sevenseg1;
+
+
 // LT24 Display Interface
 wire        LT24Wr_n;
 wire        LT24Rd_n;
@@ -51,6 +62,7 @@ MiniProject  dut (
    .clock       ( clock       ),
    .globalReset ( reset       ),
    .resetApp    ( resetApp    ),
+   .n_key       ( n_key       ),
    
    .LT24Wr_n    ( LT24Wr_n    ),
    .LT24Rd_n    ( LT24Rd_n    ),
@@ -58,7 +70,9 @@ MiniProject  dut (
    .LT24RS      ( LT24RS      ),
    .LT24Reset_n ( LT24Reset_n ),
    .LT24Data    ( LT24Data    ),
-   .LT24LCDOn   ( LT24LCDOn   )
+   .LT24LCDOn   ( LT24LCDOn   ),
+   .n_sevenseg0 ( n_sevenseg0 ),
+   .n_sevenseg1 ( n_sevenseg1 )
 );
 
 //
@@ -77,57 +91,83 @@ LT24FunctionalModel #(
     .LT24Reset_n ( LT24Reset_n ),
     .LT24Data    ( LT24Data    ),
     .LT24LCDOn   ( LT24LCDOn   ),
-    .frame       (frame)
+    .frame       ( frame )
 );
 
 //
 // Test Bench Logic
 //
 
+// Task to press a button for hold_seconds_frac seconds, release it, and
+// wait for an additional hold_seconds_frac seconds.
+task pressAndRelease(input [3:0] keyval, input [4:0] hold_seconds_frac);
+    begin
+        key<= keyval;
+        repeat(CLOCK_FREQ/hold_seconds_frac)@(posedge clock);
+        key <= 4'b0000;
+        repeat(CLOCK_FREQ/hold_seconds_frac)@(posedge clock);
+    end
+endtask
+
+
 integer fd;
+integer i;
+localparam  SEQ_LENGTH = 6;
+reg [SEQ_LENGTH*4:0] testsequence;
 
 initial begin
-    $display("%d ns\tSimulation Started",$time);       //Print to console that the simulation has started
-    reset = 1'b1;                                      //Start in reset.
-    repeat(2) @(posedge clock);                        //Wait for a couple of clock cycles
-    reset = 1'b0;                                      //Then clear the reset signal.
-    wait(resetApp === 1'b0);                           //Wait until the resetApp signal is zero.
-    $display("%d ns\tInitialisation Complete",$time);  //Print to console that initialisation of the display is complete.
+    $display("%d ns\tSimulation Started",$time);      
+    reset = 1'b1;                                     
+    repeat(2) @(posedge clock);                       
+    reset = 1'b0;                                     
+    wait(resetApp === 1'b0);                          
+    $display("%d ns\tInitialisation Complete",$time); 
     $display("STARTRENDER,",$time,";");
+    repeat(CLOCK_FREQ*3)@(posedge clock);
+
+    // A random test sequence.
+    testsequence = {4'd1,4'd8,4'd4,4'd2,4'd1,4'd8};
+
+    // Loop from 0 to SEQ_LENGTH, and apply that stimulus to the KEY input.
+    for (i = 0; i<SEQ_LENGTH;i=i+1) begin
+        pressAndRelease(testsequence[i*4+:4], 1);
+    end
+
+    // Press a button 1/20 of a second to advance modes.
+    pressAndRelease(4'b0001,20);
+    pressAndRelease(4'b0000,1);
+
+    // Perform the sequence again.
+    for (i = 0; i<SEQ_LENGTH;i=i+1) begin
+        pressAndRelease(testsequence[i*4+:4], 1);
+    end    
+
 end
 
 
 //
-//Clock generator + simulation time limit.
+//Clock generator + simulation / frame number time limit.
 //
-
 initial begin
-    clock = 1'b0; //Initialise the clock to zero.
+    clock = 1'b0;
 end
 
-//Next we convert our clock period to nanoseconds and half it
-//to work out how long we must delay for each half clock cycle
 real HALF_CLOCK_PERIOD = (1000000000.0 / $itor(CLOCK_FREQ)) / 2.0;
 
-//Now generate the clock
 integer half_cycles = 0;
 always begin
-    // if ((((100*half_cycles/2)/NUM_CYCLES) % 10) == 0) $display("asd"); 
-
-    //Generate the next half cycle of clock
-    #(HALF_CLOCK_PERIOD);          //Delay for half a clock period.
-    clock = ~clock;                //Toggle the clock
-    half_cycles = half_cycles + 1; //Increment the counter
+    #(HALF_CLOCK_PERIOD);         
+    clock = ~clock;               
+    half_cycles = half_cycles + 1;
     
-    //Check if enough half clock cycle
     if (half_cycles == (2*NUM_CYCLES)) begin 
-        //Once the number of cycles has been reached
-		half_cycles = 0; 		   //Reset half cycles, so if we resume running with "run -all", we perform another chunk.
+		half_cycles = 0;
         $display("ENDRENDER, t=%d, cyc = %d",$time, NUM_CYCLES);
-        $stop;                     //Break the simulation
-        //Note: We can continue the simulation after this breakpoint using "run -continue" or "run x ns" or "run -all" in modelsim.
+        $stop;
     end
-    if (frame>NUM_FRAMES) begin
+
+    // If NUM_FRAMES have been written, exit the simulation early.
+    if (frame==NUM_FRAMES) begin
         $display("FRAME, cyc = %d", half_cycles/2);
         $stop;
     end
